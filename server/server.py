cdfa0c6e539c2c416e1dc09db0d28b0ae575d936
Ilan Mitnikov <ilanmitnikov@gmail.com>
2022-01-22 11:36:50 +0200

    return plot data in chunks + error handling in test

--- server/server.py
+++ server/server.py
@@ -1,11 +1,15 @@
+import pandas as pd
+
 from agents.train import *
 from data.download import download
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session, Response
 from stable_baselines3.common.logger import *
 from enum import Enum
+import time
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 
 @app.route("/")
@@ -30,29 +34,37 @@ def training_status(param_name: str, file_path: str):
         return jsonify({param_name: TrainingStatus.DID_NOT_START_TRAINING.value})
 
 
-# TODO: might want to remove is_training and leave just training_step
+MODEL_FILE_PATH = "model"
 IS_TRAINING_FILE_PATH = "is_training"
+TRAINING_STEP_FILE_PATH = "training_step"
 
 
 @app.route("/is_training")
 def is_training():
     return training_status("is_training", IS_TRAINING_FILE_PATH)
-
-
-TRAINING_STEP_FILE_PATH = "training_step"
+    # return training_status("is_training", session["is_training_path"])
 
 
 @app.route("/training_step")
 def training_step():
     return training_status("training_step", TRAINING_STEP_FILE_PATH)
+    # return training_status("training_step", session["training_step_path"])
 
 
-MODEL_FILE_PATH = "model"
+def initialize_session(msg=None):
+    sess_id = time.time_ns()
+
+    session["sess_id"] = sess_id
+    session["is_training_path"] = IS_TRAINING_FILE_PATH + "_" + str(sess_id)
+    session["training_step_path"] = TRAINING_STEP_FILE_PATH + "_" + str(sess_id)
+    session["model_path"] = MODEL_FILE_PATH + "_" + str(sess_id)
+    # return Response(msg, status=200)
 
 
 @app.route("/train", methods=["POST"])
 def train():
     if request.method == "POST":
+        initialize_session()
         train_config = request.get_json()
         try:
             env = SingleAssetEnv(train_config)
@@ -70,24 +82,46 @@ def train():
         agent.learn(
             num_train_steps,
             callback=[
+                # IsTrainingCallback(TrainingStatus, file_path=session["is_training_path"]),
+                # TrainingStepCallback(TrainingStatus, file_path=session["training_step_path"]),
+
+                # TODO: might want to remove is_training and leave just training_step
                 IsTrainingCallback(TrainingStatus, file_path=IS_TRAINING_FILE_PATH),
                 TrainingStepCallback(TrainingStatus, file_path=TRAINING_STEP_FILE_PATH),
             ],
         )
+        # agent.save(session["model_path"])
         agent.save(MODEL_FILE_PATH)
 
-    return jsonify(success=True)
+    return jsonify(id=session["sess_id"])
 
 
 @app.route("/test", methods=["POST"])
 def test():
     if request.method == "POST":
         test_config = request.get_json()
-        test_env = SingleAssetEnv(test_config)
+        try:
+            test_env = SingleAssetEnv(test_config)
+        except AttributeError:
+            download(
+                [test_config["symbol"]],
+                [test_config["interval"]],
+                test_config["start"],
+                test_config["end"],
+            )
+            test_env = SingleAssetEnv(test_config)
         check_env(test_env)
         obs = test_env.reset()
         agent = SingleDQNAgent(test_env)
-        agent.load(MODEL_FILE_PATH)
+
+        try:
+            # agent.load(MODEL_FILE_PATH + "_" + str(test_config["id"]))
+            agent.load(MODEL_FILE_PATH)
+        except FileNotFoundError:
+            return Response(
+                "Agent model file not found, try training first.", status=500
+            )
+
         num_test_steps = test_config.get("num_steps", np.inf)
         while test_env.step_idx <= num_test_steps:
             action = agent.predict(obs)
@@ -95,12 +129,28 @@ def test():
             if done:
                 break
 
-        resp = jsonify(
-            balances=agent.env.ledger.balances,
-            timestamps=agent.env.ledger.dates,
-            candles=agent.env.df.to_json(),
-        )
-        return resp
+        def generate(df: pd.DataFrame):
+            """Yields the whole ledger in one chunk, then yields every candle in order"""
+
+            yield '{"ledger":'
+            yield json.dumps(
+                dict(
+                    balances=agent.env.ledger.balances,
+                    timestamps=agent.env.ledger.dates,
+                ),
+                indent=4,
+                sort_keys=True,
+                default=str,
+            )
+            yield ',"candles":{'
+            for i in range(len(df)):
+                yield f'"{i}":'
+                yield df.iloc[i].to_json()
+                if i != len(df) - 1:
+                    yield ","
+            yield "}}"
+
+        return Response(generate(agent.env.df), mimetype="application/json")
 
 
 app.run(host="127.0.0.1", port=5000)
