69de804baa8332b1e7c22decfb6a33dbd9ea8bdc
Ido Calman <calman.ido@gmail.com>
2022-04-10 21:40:17 -0400

    Train agents added to evaluation table

--- client/src/views/Evaluate.js
+++ client/src/views/Evaluate.js
@@ -1,51 +1,46 @@
+import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import './Evaluate.css';
 
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
-];
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { field: 'train_start', headerName: 'Start Date', width: 130 },
+    { field: 'train_end', headerName: 'End Date', width: 130 },
+    { field: 'train_interval', headerName: 'Interval', width: 70 },
+    { field: 'symbols', headerName: 'Assets', width: 150 }
 ];
 
 function Evaluate() {
+
+    let [agents, setAgents] = React.useState([]);
+
+    React.useEffect(() => {
+      // declare the async data fetching function
+      const fetchData = async () => {
+          const URL = '/api/agent/all';
+          const data = await fetch(URL);
+          const json = await data.json();
+          let res = json.content;
+          console.log(res);
+          setAgents(res);
+      }
+
+      // call the function
+      fetchData()
+        // make sure to catch any error
+        .catch(console.error);;
+    }, [])
+
     return (
         <div className="evaluate">
             <div className="main-space">
                 <div className="box">
                     <div className="title">
-                        <h2>Evaluation Settings</h2>
+                        <h2>Agents</h2>
                     </div>
                     <div className="agents-table" style={{height: 400, width: '100%' }}>
                         <DataGrid
-                            rows={rows}
+                            rows={agents}
                             columns={columns}
                             pageSize={10}
                         />
@@ -54,7 +49,7 @@ function Evaluate() {
             </div>
             <div className="box right-space">
                 <div className="title">
-                    <h2>Summary</h2>
+                    <h2>Evaluate</h2>
                 </div>
                 <div className="details">
                 </div>
